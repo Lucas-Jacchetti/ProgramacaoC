717f19a26dcc5ef2c911011d0787e4066e6d90f4
Lucas-Jacchetti <155106313+Lucas-Jacchetti@users.noreply.github.com>
2026-04-09 18:56:51 +0000

    exercicios

--- 02-aula-2603/src/main.c
+++ 02-aula-2603/src/main.c
@@ -4,6 +4,21 @@
 
 void quadrado(int* a);
 void trocar(int* a, int* b);
+void avaliarAluno(float n1, float n2, float n3, float* media, int* situacao);
+
+void avaliarAluno(float n1, float n2, float n3, float* media, int* situacao){
+    n1 = n1 + n2 + n3;
+    *media = n1/3;
+    if (*media < 4){
+        *situacao = 3;
+    }
+    else if (*media >= 4 && *media < 7){
+        *situacao = 2;
+    }
+    else{
+        *situacao = 1;
+    }
+}
 
 void trocar(int* a, int* b){
     int temp = *a;
@@ -16,12 +31,26 @@ void quadrado(int* a){
 }
 
 int main() {
-    int a = 10;
+    //int a = 10;
+
+    float n1 = 5.1;
+    float n2 = 2.7;
+    float n3 = 8.8;
+    float media;
+    int situacao;
+
     //int b = 20;
     //printf("a: %d, b: %d\n", a, b);
+    
     //trocar(&a, &b);
     //printf("a: %d, b: %d\n", a, b);
-    quadrado(&a);
-    printf("a: %d\n", a);
+    
+    //quadrado(&a);
+    //printf("a: %d\n", a);
+    
+    avaliarAluno(n1, n2, n3, &media, &situacao);
+    printf("media: %.2f\n", media);
+    printf("situação: %d\n", situacao);
+    
     return 0;
 }
